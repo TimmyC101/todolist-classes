b9c97d7177b303cfbcd944d6b6b39fd9cfd0df9d
TimmyC101 <tcalhoun101@yahoo.com>
2020-01-15 20:29:43 -0600

    Updates

---
+++ precache-manifest.63e025e97ccf9957269ffb5cedb0268f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43617f4f902998ef9dafd3eb23365b2",
+    "url": "/todolist-classes/index.html"
+  },
+  {
+    "revision": "0c067c9ce8e49f29a533",
+    "url": "/todolist-classes/static/css/main.bb601bfe.chunk.css"
+  },
+  {
+    "revision": "d76774655373ec916e6f",
+    "url": "/todolist-classes/static/js/2.da1a7f95.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist-classes/static/js/2.da1a7f95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c067c9ce8e49f29a533",
+    "url": "/todolist-classes/static/js/main.dfb856b8.chunk.js"
+  },
+  {
+    "revision": "d874f71d49e6eb71ba01",
+    "url": "/todolist-classes/static/js/runtime-main.628d9261.js"
+  }
+]);
